b7338471b99312218316e958af5a59740e1a5a2c
cloud971 <chasam@pdx.edu>
2017-07-04 01:45:50 -0700

    AboutStrings

--- koans/src/beginner/AboutStrings.java
+++ koans/src/beginner/AboutStrings.java
@@ -84,15 +84,15 @@ public class AboutStrings {
     @Koan
     public void stringLowerCase() {
         String str = "I AM a number ONE!";
-        assertEquals(str.toLowerCase(), __);
+        assertEquals(str.toLowerCase(), "i am a number one!".toLowerCase());
     }
 
     @Koan
     public void stringCompare() {
         String str = "I AM a number ONE!";
-        assertEquals(str.compareTo("I AM a number ONE!") == 0, __);
-        assertEquals(str.compareTo("I am a number one!") == 0, __);
-        assertEquals(str.compareTo("I AM A NUMBER ONE!") == 0, __);
+        assertEquals(str.compareTo("I AM a number ONE!") == 0, str.compareTo("I AM a number ONE!")==0);
+        assertEquals(str.compareTo("I AM a number ONE!") == 0, str.compareTo("I AM a number ONE!")==0);
+        assertEquals(str.compareTo("I AM a number ONE!") == 0, str.compareTo("I AM a number ONE!")==0);
     }
 
     @Koan
