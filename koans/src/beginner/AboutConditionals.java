7d93605cd076e148e3cf284845fb3185fa7d97df
cloud971 <chasam@pdx.edu>
2017-07-05 00:30:53 -0700

    AboutConditionals

--- koans/src/beginner/AboutConditionals.java
+++ koans/src/beginner/AboutConditionals.java
@@ -79,7 +79,7 @@ public class AboutConditionals {
             default:
                 result += "Nothing";
         }
-        assertEquals(result, );
+        assertEquals(result, "Basic OneTwoNothing");
     }
 
     @Koan
