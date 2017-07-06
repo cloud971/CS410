79c400ecfd8d79051d4a9838267f47bbd5ade79f
cloud971 <chasam@pdx.edu>
2017-07-05 23:42:15 -0700

    AboutPtypes

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -34,7 +34,7 @@ public class AboutPrimitives {
 
     @Koan
     public void wholeNumbersCanAlsoBeOfTypeLong() {
-        assertEquals(getType(1L), getType(2^63));
+        assertEquals(getType(1L), getType(2^64));
     }
 
     @Koan
