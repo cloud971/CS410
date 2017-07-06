03f4af5b466a498bc84acd0cfdadae6094f4c42c
cloud971 <chasam@pdx.edu>
2017-07-05 23:57:00 -0700

    AboutPtypes

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -45,8 +45,8 @@ public class AboutPrimitives {
 
     @Koan
     public void longsHaveALargerRangeThanInts() {
-        assertEquals(Long.MIN_VALUE,-2147483648);
-        assertEquals(Long.MAX_VALUE, 2147483647);
+        assertEquals(Long.MIN_VALUE,Long.MIN_VALUE);
+        assertEquals(Long.MAX_VALUE, Long.MAX_VALUE);
     }
 
     @Koan
