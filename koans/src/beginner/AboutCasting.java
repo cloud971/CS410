5235c6c2a1d6452af0022e9022d3b5e6b22b9dd1
cloud971 <chasam@pdx.edu>
2017-07-07 02:00:23 -0700

    aboutcasting

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -98,7 +98,7 @@ public class AboutCasting {
     public void classCasting() {
         try {
             Object o = new Object();
-            ((Sleepable) o).sleep(); // would this even compile without the cast?
+            ((Grandparent) o).sleep(); // would this even compile without the cast?
         } catch (ClassCastException x) {
             fail("Object does not implement Sleepable, maybe one of the people classes do?");
         }
