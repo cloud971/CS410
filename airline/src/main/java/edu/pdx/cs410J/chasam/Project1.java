9105f497b20b809d57037f58440f998069b645e1
cloud971 <chasam@pdx.edu>
2017-07-06 22:00:56 -0700

    added comment

--- airline/src/main/java/edu/pdx/cs410J/chasam/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/chasam/Project1.java
@@ -10,6 +10,8 @@ public class Project1 {
   public static void main(String[] args) {
     Class c = AbstractAirline.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
     System.err.println("Missing command line arguments");
+
+    /*loops string and displays*/
     for (String arg : args) {
       System.out.println(arg);
     }
