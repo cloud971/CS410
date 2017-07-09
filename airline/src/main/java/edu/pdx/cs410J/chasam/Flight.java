8d58a112a47b99a19b4ce76d060f37294be9087f
cloud971 <chasam@pdx.edu>
2017-07-09 11:19:09 -0700

    project 1

--- airline/src/main/java/edu/pdx/cs410J/chasam/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/chasam/Flight.java
@@ -4,19 +4,28 @@ import edu.pdx.cs410J.AbstractFlight;
 
 public class Flight extends AbstractFlight {
 
-    private String [] Flight_data = new String[8];
+    private String name;
+    private String flightNumber;
+    private String Src;
+    private String arrival;
+    private String dest;
+    private String dep;
+
+    public Flight(){
+
+    }
 
   public Flight(String [] data){
 
     // no print or readme
-    if(data.length == Flight_data.length){
+    if(data.length == 8){
 
-      this.Flight_data[0] = new String(data[0]); // name
-      this.Flight_data[1] = new String(data[1]); // flight number
-      this.Flight_data[2] = new String(data[2]); // 3 symbol
-      this.Flight_data[3] = new String(data[3] + data[4]); // data and time arrival
-      this.Flight_data[4] = new String(data[5]); // destination
-      this.Flight_data[5] = new String(data[6] + data[7]); // data and time of dep
+      this.name = new String(data[0]); // name
+      this.flightNumber = new String(data[1]); // flight number
+      this.Src = new String(data[2]); // 3 symbol
+      this.arrival = new String(data[3] + data[4]); // data and time arrival
+      this.dest = new String(data[5]); // destination
+      this.dep = new String(data[6] + data[7]); // data and time of dep
 
     }
 
@@ -30,9 +39,9 @@ public class Flight extends AbstractFlight {
   @Override
   public int getNumber() {
 
-    int flight_num = Integer.parseInt(Flight_data[1]);
+    int flightNum = Integer.parseInt(flightNumber);
 
-    return flight_num;
+    return flightNum;
   }
 
   @Override
