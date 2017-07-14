3c821e31319b56005cb1a190bbf4f9e0e0c4fad4
cloud971 <chasam@pdx.edu>
2017-07-13 23:09:41 -0700

    dsfs

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -108,7 +108,7 @@ public class AboutCasting {
     public void complicatedCast() {
         Grandparent parent = new Parent();
         // How can we access the parent's ability to "complain" - if the reference is held as a superclass?
-        assertEquals("TPS reports don't even have a cover letter!", __);
+        assertEquals("TPS reports don't even have a cover letter!",((Parent)parent).complain());
     }
 
 }
