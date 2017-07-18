6987bfa6d582905dce881bcb72a75137326bb7c0
cloud971 <chasam@pdx.edu>
2017-07-17 22:40:05 -0700

    project 1

--- airline/src/main/java/edu/pdx/cs410J/chasam/Project2.java
+++ airline/src/main/java/edu/pdx/cs410J/chasam/Project2.java
@@ -7,7 +7,7 @@ import java.text.*;
 /**
  * The main class for the CS410J airline Project
  */
-public class Project1 {
+public class Project2 {
 
   public static void main(String[] args) {
     Class c = AbstractAirline.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
