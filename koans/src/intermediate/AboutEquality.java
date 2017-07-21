0c80e5c208756c01b5d16845f1babf8484c72b45
cloud971 <chasam@pdx.edu>
2017-07-20 22:12:14 -0700

    dsfs

--- koans/src/intermediate/AboutEquality.java
+++ koans/src/intermediate/AboutEquality.java
@@ -12,15 +12,15 @@ public class AboutEquality {
     public void sameObject() {
         Object a = new Object();
         Object b = a;
-        assertEquals(a == b, __);
+        assertEquals(a == b, true);
     }
 
     @Koan
     public void equalObject() {
         Integer a = new Integer(1);
         Integer b = new Integer(1);
-        assertEquals(a.equals(b), __);
-        assertEquals(b.equals(a), __);
+        assertEquals(a.equals(b), true);
+        assertEquals(b.equals(a), true);
     }
 
     @Koan
@@ -39,16 +39,20 @@ public class AboutEquality {
 
         @Override
         public boolean equals(Object other) {
-            // Change this implementation to match the equals contract
-            // Car objects with same horsepower and name values should be considered equal
-            // http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#equals(java.lang.Object)
+
+            if (!(other instanceof Car)) {
+                return false;
+            }
+            if (this.hashCode() == other.hashCode()) {
+                return true;
+            }
             return false;
         }
 
         @Override
         public int hashCode() {
-            // @see http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#hashCode()
-            return super.hashCode();
+
+            return (this.name + this.horsepower).hashCode();
         }
     }
 
@@ -56,7 +60,6 @@ public class AboutEquality {
     public void equalForOwnObjects() {
         Car car1 = new Car("Beetle", 50);
         Car car2 = new Car("Beetle", 50);
-        // @see Car.equals (around line 45) for the place to solve this
         assertEquals(car1.equals(car2), true);
         assertEquals(car2.equals(car1), true);
     }
@@ -65,7 +68,6 @@ public class AboutEquality {
     public void unequalForOwnObjects() {
         Car car1 = new Car("Beetle", 50);
         Car car2 = new Car("Porsche", 300);
-        // @see Car.equals (around line 45) for the place to solve this
         assertEquals(car1.equals(car2), false);
     }
 
@@ -73,25 +75,20 @@ public class AboutEquality {
     public void unequalForOwnObjectsWithDifferentType() {
         Car car1 = new Car("Beetle", 50);
         String s = "foo";
-        // @see Car.equals (around line 45) for the place to solve this
         assertEquals(car1.equals(s), false);
     }
 
     @Koan
     public void equalNullForOwnObjects() {
         Car car1 = new Car("Beetle", 50);
-        // @see Car.equals (around line 45) for the place to solve this
         assertEquals(car1.equals(null), false);
     }
 
     @Koan
     public void ownHashCode() {
-        // As a general rule: When you override equals you should override
-        // hash code
-        // Read the hash code contract to figure out why
-        // http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#hashCode()
 
-        // Implement Car.hashCode around line 51 so that the following assertions pass
+
+
         Car car1 = new Car("Beetle", 50);
         Car car2 = new Car("Beetle", 50);
         assertEquals(car1.equals(car2), true);
@@ -121,8 +118,6 @@ public class AboutEquality {
         Chicken chicken2 = new Chicken();
         assertEquals(chicken1.equals(chicken2), __);
         assertEquals(chicken1.hashCode() == chicken2.hashCode(), __);
-        // Does this still fit the hashCode contract? Why (not)?
-        // Fix the Chicken class to correct this.
     }
 
 }
