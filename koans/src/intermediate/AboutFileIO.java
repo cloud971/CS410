af47993fda0d7761855710683b8405d765af4561
cloud971 <chasam@pdx.edu>
2017-07-20 22:21:02 -0700

    dsfs

--- koans/src/intermediate/AboutFileIO.java
+++ koans/src/intermediate/AboutFileIO.java
@@ -13,7 +13,7 @@ public class AboutFileIO {
     @Koan
     public void fileObjectDoesntCreateFile() {
         File f = new File("foo.txt");
-        assertEquals(f.exists(), true);
+        assertEquals(f.exists(), false);
     }
 
     @Koan
