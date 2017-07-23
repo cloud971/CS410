d8abae0eb3c1a227102986dfaf9f389426fc5fd2
cloud971 <chasam@pdx.edu>
2017-07-23 14:30:06 -0700

    dsfs

--- koans/src/intermediate/AboutSerialization.java
+++ koans/src/intermediate/AboutSerialization.java
@@ -162,7 +162,7 @@ public class AboutSerialization {
         try {
             is = new ObjectInputStream(new FileInputStream("SerializeFile"));
             Dog otherDog = (Dog) is.readObject();
-            assertEquals(otherDog.name, "Snoopy");
+            assertEquals(otherDog.name, "snoopy");
         } finally {
             closeStream(is);
         }
@@ -200,7 +200,7 @@ public class AboutSerialization {
             is = new ObjectInputStream(new FileInputStream("SerializeFile"));
             MilitaryPlane otherPlane = (MilitaryPlane) is.readObject();
             // Does this surprise you?
-            assertEquals(otherPlane.name, __);
+            assertEquals(otherPlane.name, null);
 
             // Think about how serialization creates objects...
             // It does not use constructors! But if a parent object is not serializable
